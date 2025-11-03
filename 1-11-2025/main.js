b92f583cbc911bd4be28156ab3b2ea0ced7ed443
ShadowJod <nikhilmishra0187@gmail.com>
2025-11-03 22:54:11 +0530

    1-11-2025 Completed

---
+++ 1-11-2025/main.js
@@ -0,0 +1,93 @@
+// let val = prompt("Enter your age");
+
+// if(age>=18) console.log("Eligible");
+// else console.log("Not Eligible");
+
+
+
+// for(let i=1; i<11; i++){
+//     console.log(`5 * ${i} = ${5*i}`);
+
+// }
+
+// let count = 0;
+
+// for(let i=1; i<16; i++){
+//     if(i>8) count++;
+// }
+
+// console.log(count);
+
+// let pass = prompt("Enter your Password");
+
+// if("Nikhil@123"=== pass) console.log("Granted");
+// else console.log("Wrong Password")
+// 
+
+
+// let chance = 0;
+
+// for(let i= 0; i<3; i++){
+//     let pass = prompt("Enter Passeword");
+//     if(pass=="Nikhil@123"){
+//         console.log("Granted");
+//         break;   
+//     }
+//     else{
+//         chance++;
+//         console.log("Wrong Password");
+        
+//         if(chance===3){
+//             console.log("Your Account is freeze");
+//             break;
+//         }
+//     }
+// }
+
+
+// let a = prompt("Enter 1st Number");
+// let b = prompt("Enter 2nd Number");
+// a = +a;
+// b = +b;
+
+// for(let i=a; i<b-1; i++){
+//     console.log(i+1);
+// }
+
+
+// let odd = 0;
+// let even = 0;
+// let positive = 0;
+// let negative = 0;
+
+// for(let a = 0; a<5; a++){
+//     let val = prompt("Enter a Number: ");
+//     val = +val;
+//     if(val>=0) positive++;
+//     else  negative++;
+// }
+
+
+// console.log(positive);
+// console.log(negative);
+
+// let availableBalance = 1000;
+
+// for(let i=0; i<3; i++){
+//     let amount = prompt("Enter the amount");
+
+//     if(amount>availableBalance){ 
+//         console.log("Insuuficient Balance");
+//         break;
+//     }
+
+//     else availableBalance = availableBalance -amount;
+
+// }
+
+// console.log(availableBalance);
+
+
+
+
+
---
+++ 29-10-2025/main.js
@@ -0,0 +1,41 @@
+// 1. Basic Operators (Arithmetic, Assignment, Increment, Decrement,
+// Comparison, Logical, Bitwise)
+
+// a. Create two numbers a = 10 and b = 3.
+// Perform and log: a + b, a - b, a * b, a / b, a % b.
+// b. Write: let x = 5; x = x + 3;
+// Now rewrite the same using +=.
+// Do the same for -=, *=, /=.
+// c. let count = 5;
+// Use count++ and log value before and after.
+// Repeat for count–.
+// d. Compare two values: 5 == “5” and 5 === “5”.
+// Observe difference.
+// e. Check if 10 is greater than 5, less than 20, and equal to 10.
+// f. Try logical AND and OR:
+// true && false
+// true || false
+// !(true)
+// g. Predict the result of:
+// (5 > 3 && 10 > 8),
+// (5 > 3 || 10 < 8)
+// h. Bitwise (light intro):
+// Evaluate 5 & 1 and 5 | 1.
+// Write result and your observation (no deep explanation needed now).
+
+
+// Answer
+let a = 10;
+let b = 3;
+
+console.log(a+b); //13
+console.log(a-b); //7
+console.log(a/b); //3.3
+console.log(a*b); // 30
+console.log(a%b); //1
+
+
+
+
+
+
